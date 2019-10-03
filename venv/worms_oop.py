daad0a6a1b674f563369654176ee1f5d04a3edef
Alexander Kowsik <alexander.kowsik@gmx.de>
2019-10-03 18:20:00 +0200

    implemented canons, click changes player

--- venv/worms_oop.py
+++ venv/worms_oop.py
@@ -4,7 +4,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import numpy as np
 import random
-from math import sqrt, sin, pi
+from math import pi
 import skfmm
 import matplotlib.pyplot as plt
 
@@ -16,82 +16,149 @@ HEIGHT = 600
 class Worms:
     def __init__(self):
         self.display = QLabel()
-        self.display.mouseEvent = self.mouse_event
+        self.display.setMouseTracking(True)
+        self.display.mouseMoveEvent = self.mouse_move_event
+        self.display.mousePressEvent = self.mouse_press_event  # Klick wechselt Spieler
+
         self.W = np.linspace(0.001, 0.05, 20)  # W war vorgegeben
         self.curve = self.create_curve()
+
+        self.player1Pos = QPoint(200, self.curve[200])
+        self.player2Pos = QPoint(1000, self.curve[1000])
+        self.currentPlayer = 1
+
         self.worldImg = self.create_world_image()
         self.mappainter = QPainter(self.worldImg)
-        self.make_crater()  # demo
+
         self.charsImg = self.create_chars_image()
-        self.charpainter = QPainter(self.charsImg)
-        self.create_chars()
+        self.canonImg = self.create_canon_image()
+
+        self.make_crater(500, 50)  # demo: x = 500, radius = 50
+        self.draw_charsImg()
+
         # self.windVectors = self.get_wind_vector_field()
-        self.redraw()
+        self.redraw(self.player1Pos.x(), 0)
 
     def create_curve(self):
-        random1 = [random.uniform(0, 2 * pi) for i in range(len(self.W))]
-        random2 = [random.uniform(-1, 1) for i in range(len(self.W))]
-        return [self.get_curve(i, random1, random2) for i in range(WIDTH)]
+        random1 = [random.uniform(0, 2 * pi) for _ in range(len(self.W))]
+        random2 = [random.uniform(-1, 1) for _ in range(len(self.W))]
+        return [self.get_curve_fx(i, random1, random2) for i in range(WIDTH)]
 
-    def get_curve(self, x, r1, r2):
+    def get_curve_fx(self, x, r1, r2):
         fx = 0
         for i in range(len(self.W)):
-            fx += 1 / sqrt(self.W[i]) * sin(self.W[i] * x + r1[i]) * r2[i]
+            fx += 1 / np.sqrt(self.W[i]) * np.sin(self.W[i] * x + r1[i]) * r2[i]
         return fx * 3 + 350  # willkürliche Skalierung (Ausprobieren)
 
     def create_world_image(self):
-        m = self.create_bool_landscape()
+        land = self.create_bool_landscape()
 
         # färbt den Boden entsprechend der Distanz zur Oberfläche
-        map_dist = skfmm.distance(m)
+        map_dist = skfmm.distance(land)
         map_dist = map_dist / np.max(map_dist) * 4  # willkürlicher Faktor (alternativ 0.6 draufaddieren)
         world = plt.cm.copper_r(map_dist)
-        world[:, :, 3] = m
+        world[:, :, 3] = land
         world = np.asarray(world * 255, np.uint8)
         return QImage(world, WIDTH, HEIGHT, QImage.Format_RGBA8888)
 
     def create_bool_landscape(self):
-        m = np.zeros([HEIGHT, WIDTH], dtype=np.bool)
+        land = np.zeros([HEIGHT, WIDTH], dtype=np.bool)
 
         # prüfe für jeden Pixel, ob er unter dem Funktionswert an der x-Stelle liegt
         for i in range(WIDTH):
             for j in range(HEIGHT):
                 if j > self.curve[i]:
-                    m[j, i] = True
+                    land[j, i] = True
                 else:
-                    m[j, i] = False
-        return m
+                    land[j, i] = False
+        return land
 
-    def make_crater(self):
+    def make_crater(self, x, radius):
         self.mappainter.setCompositionMode(QPainter.CompositionMode_Clear)
         self.mappainter.setPen(Qt.white)
         self.mappainter.setBrush(Qt.white)
 
         # zeichnet Krater an die Funktionsstelle von 500 mit Radius 50
-        self.mappainter.drawEllipse(QPoint(500, self.curve[500]), 50, 50)
+        self.mappainter.drawEllipse(QPoint(x, self.curve[x]), radius, radius)
 
     def create_chars_image(self):
         chars = np.zeros([WIDTH, HEIGHT, 4])
         chars[:, :, 3] = 0  # macht Ebene transparent
-        return QImage(chars, WIDTH, HEIGHT, QImage.Format_RGBA8888)
+        img = QImage(chars, WIDTH, HEIGHT, QImage.Format_RGBA8888)
+        charpainter = QPainter(img)
 
-    def create_chars(self):
         # erster Spieler
-        self.charpainter.setPen(Qt.green)
-        self.charpainter.setBrush(Qt.green)
-        self.charpainter.drawEllipse(QPoint(200, self.curve[200]), 15, 15)
+        charpainter.setPen(Qt.green)
+        charpainter.setBrush(Qt.green)
+        charpainter.drawEllipse(QPoint(200, self.curve[200]), 15, 15)
 
         # zweiter Spieler
-        self.charpainter.setPen(Qt.blue)
-        self.charpainter.setBrush(Qt.blue)
-        self.charpainter.drawEllipse(QPoint(1000, self.curve[1000]), 15, 15)
+        charpainter.setPen(Qt.blue)
+        charpainter.setBrush(Qt.blue)
+        charpainter.drawEllipse(QPoint(1000, self.curve[1000]), 15, 15)
 
-    def redraw(self):
-        # zeichne Ebene mit Spielern auf die map
+        return img
+
+    def draw_charsImg(self):
         self.mappainter.setCompositionMode(QPainter.CompositionMode_SourceOver)
         self.mappainter.drawPixmap(0, 0, QPixmap.fromImage(self.charsImg))
 
-        self.display.setPixmap(QPixmap.fromImage(self.worldImg))
+    def create_canon_image(self):
+        canon = np.zeros([7, 60, 4])
+        canon[:, :, 3] = 0
+        img = QImage(canon, 7, 60, QImage.Format_RGBA8888)
+        canonpainter = QPainter(img)
+        canonpainter.setPen(QColor(qRgb(0, 52, 135)))
+        canonpainter.setBrush(QColor(qRgb(0, 52, 135)))
+        canonpainter.drawRect(0, 0, 7, 30)
+        canonpainter.drawRect(7, 30, 7, 30)
+        return img
+
+    def get_angle(self, x, y):
+        px = self.player1Pos.x() if self.currentPlayer == 1 else self.player2Pos.x()
+        py = self.player1Pos.y() if self.currentPlayer == 1 else self.player2Pos.y()
+
+        if x == px:
+            if y <= py:
+                return 0
+            else:
+                return 180
+        elif y == py:
+            if x <= py:
+                return 90
+            else:
+                return -90
+        elif x < px:
+            if y < py:
+                return np.degrees(np.arctan((px - x) / (py - y)))
+            else:
+                return 180 + np.degrees(np.arctan((px - x) / (py - y)))
+        else:
+            if y < py:
+                return 360 + np.degrees(np.arctan((px - x) / (py - y)))
+            else:
+                return 180 - np.degrees(np.arctan((px - x) / (y - py)))
+
+    def redraw(self, x, y):
+        img = self.worldImg.copy()
+        painter = QPainter(img)
+
+        painter.setPen(QColor(qRgb(0, 52, 135)))
+        painter.setBrush(QColor(qRgb(0, 52, 135)))
+        painter.setCompositionMode(QPainter.CompositionMode_SourceOver)
+
+        c = QPointF(self.player1Pos.x(), self.player1Pos.y()) if self.currentPlayer == 1 else \
+            QPointF(self.player2Pos.x(), self.player2Pos.y())
+
+        painter.translate(c)
+        angle = self.get_angle(x, y)
+        painter.rotate(-angle + 180)
+        painter.drawPixmap(QPoint(-4, 0), QPixmap.fromImage(self.canonImg))
+        painter.rotate(-180 + angle)
+        painter.translate(-c)
+        painter.end()
+
+        self.display.setPixmap(QPixmap.fromImage(img))
         self.display.show()
 
     # TODO: die Funktion ist wahrscheinlich doch Blödsinn
@@ -103,18 +170,23 @@ class Worms:
         windvector = np.zeros([HEIGHT, WIDTH])
 
         for i in range(WIDTH):
-            tmp = self.get_curve(i, r1, r2)
+            tmp = self.get_curve_fx(i, r1, r2)
             for j in range(HEIGHT):
                 if j > self.curve[i]:
                     break
-                windvector[j][i] = (tmp + self.get_curve(j, r3, r4) - 700) / 6
+                windvector[j][i] = (tmp + self.get_curve_fx(j, r3, r4) - 700) / 6
         return windvector
 
-    def mouse_event(self, event):
-        pass
+    def mouse_press_event(self, event):
+        self.currentPlayer = 2 if self.currentPlayer == 1 else 1
+        print('press', event.pos())
+
+    def mouse_move_event(self, QMouseEvent):
+        self.redraw(QMouseEvent.pos().x(), QMouseEvent.pos().y())
 
 
 app = QApplication(sys.argv)
+# app.setOverrideCursor(Qt.BlankCursor)  # lässt Mauszeiger verschwinden
 Worms()
 sys.exit(app.exec_())
 
